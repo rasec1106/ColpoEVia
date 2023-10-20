011aee31aab0db44eb9545b1a16e3128eba77c80
Suite <suite@SUITESB505-ST18.local>
2023-10-19 21:20:42 -0500

    Create initial project

---
+++ Colpo e Via/Colpo_e_ViaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colpo_e_ViaApp.swift
+//  Colpo e Via
+//
+//  Created by Suite on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Colpo_e_ViaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
